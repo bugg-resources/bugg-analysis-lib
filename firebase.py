1d2cbd6f9bdf616ab352836c8c54283e22b69999
Justyn Butler <commits@justyn.co>
2023-11-15 19:34:36 +0000

    refactor: create BuggFirebase class
    BuggFirebase.db is initialised with the class
    
    Function and parameter names converted from camel case to snake case.

--- firebase.py
+++ firebase.py
@@ -5,147 +5,142 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from google.cloud import storage
 
-# TODO: make this a class so that DB instance is not global and always initialised
 
-db = None
+class BuggFirebase:
+    "A class to handle Firestore and Cloud Storage interactions"
 
+    def __init__(self, project_id: str):
+        if not firebase_admin._apps:
+            cred = credentials.ApplicationDefault()
+            firebase_admin.initialize_app(
+                cred,
+                {
+                    "projectId": project_id,
+                },
+            )
+        self.db = firestore.client()
 
-def initFirebase(project_id):
-    global db
-    if not firebase_admin._apps:
-        cred = credentials.ApplicationDefault()
-        firebase_admin.initialize_app(
-            cred,
-            {
-                "projectId": project_id,
-            },
+    def get_audio_db_record(self, audio_id: str):
+        # fetches the record we have for this audio file from the database
+        audio_ref = self.db.collection("audio").document(audio_id)
+        doc = audio_ref.get()
+        if doc.exists:
+            return doc.to_dict()
+
+        return None
+
+    def get_analysis_result(self, analysis_id: str, audio_id: str):
+        """
+        Will return the result from firestore if there is one or None if not.
+
+        Note that storing results in the palce is optional (and subject to 1mb limit). This method is just for convenience
+        """
+        results_ref = (
+            self.db.collection("audio")
+            .document(audio_id)
+            .collection(analysis_id)
+            .document("result")
         )
-    db = firestore.client()
+        doc = results_ref.get()
+        if doc.exists:
+            return doc.to_dict()
 
+        return None
 
-def getAudioDBRecord(audioId: str):
-    # fetches the record we have for this audio file from the database
-    audio_ref = db.collection("audio").document(audioId)
-    doc = audio_ref.get()
-    if doc.exists:
-        return doc.to_dict()
+    def set_analysis_result(self, analysis_id: str, audio_id: str, result: dict):
+        """
+        Store a result in the common place in firestore. Will merge into any existing result.
 
-    return None
+        (Storing in this spot is optional.)
 
+        Note no transactions are used, you may want to explore them if writing multiple entries.
+        """
+        results_ref = (
+            self.db.collection("audio")
+            .document(audio_id)
+            .collection(analysis_id)
+            .document("result")
+        )
+        results_ref.set(result, merge=True)
 
-def getAnalysisResult(analysisId: str, audioId: str):
-    """
-    Will return the result from firestore if there is one or None if not.
+    def mark_analysis_complete(self, analysis_id: str, audio_id: str, detections: list):
+        """
+        Updates the audio record to show that analysis is done (which will kick off other analyses)
+        """
+        transaction = self.db.transaction()
+        self._mark_complete_in_transaction(
+            transaction, analysis_id, audio_id, detections
+        )
 
-    Note that storing results in the palce is optional (and subject to 1mb limit). This method is just for convenience
-    """
-    results_ref = (
-        db.collection("audio")
-        .document(audioId)
-        .collection(analysisId)
-        .document("result")
-    )
-    doc = results_ref.get()
-    if doc.exists:
-        return doc.to_dict()
+    @firestore.transactional
+    def _mark_complete_in_transaction(
+        self, transaction, analysis_id: str, audio_id: str, detections: list
+    ):
+        audioRef = self.db.collection("audio").document(audio_id)
 
-    return None
+        snapshot = audioRef.get(transaction=transaction)
+        analysesPerformed = snapshot.get("analysesPerformed")
 
+        if analysis_id in analysesPerformed:
+            print(
+                f"WARNING: Analysis {analysis_id} was already completed for {audio_id}"
+            )
+        else:
+            analysesPerformed.append(analysis_id)
 
-def setAnalysisResult(analysisId: str, audioId: str, result: dict):
-    """
-    Store a result in the common place in firestore. Will merge into any existing result.
+        audioRecord = snapshot.to_dict()
+        newDetectionsList = []
 
-    (Storing in this spot is optional.)
+        # Add in the old detections and merge any of the old records
+        # We merge because the analysis may be re-run after an update
+        if "detections" in audioRecord:
+            prevDetections = snapshot.get("detections")
+            for d in prevDetections:
+                match = next((x for x in detections if d["id"] == x["id"]), None)
+                print(f"match {match}")
+                if match == None:
+                    newDetectionsList.append(d)
+                else:
+                    # Merge the two, letting the newer one overrite the old
+                    newDetectionsList.append({**d, **match})
 
-    Note no transactions are used, you may want to explore them if writing multiple entries.
-    """
-    results_ref = (
-        db.collection("audio")
-        .document(audioId)
-        .collection(analysisId)
-        .document("result")
-    )
-    results_ref.set(result, merge=True)
-
-
-def markAnalysisComplete(analysisId: str, audioId: str, detections: list):
-    """
-    Updates the audio record to show that analysis is done (which will kick off other analyses)
-    """
-    transaction = db.transaction()
-    _markCompleteInTransaction(transaction, analysisId, audioId, detections)
-
-
-@firestore.transactional
-def _markCompleteInTransaction(
-    transaction, analysisId: str, audioId: str, detections: list
-):
-    audioRef = db.collection("audio").document(audioId)
-
-    snapshot = audioRef.get(transaction=transaction)
-    analysesPerformed = snapshot.get("analysesPerformed")
-
-    if analysisId in analysesPerformed:
-        print(f"WARNING: Analysis {analysisId} was already completed for {audioId}")
-    else:
-        analysesPerformed.append(analysisId)
-
-    audioRecord = snapshot.to_dict()
-    newDetectionsList = []
-
-    # Add in the old detections and merge any of the old records
-    # We merge because the analysis may be re-run after an update
-    if "detections" in audioRecord:
-        prevDetections = snapshot.get("detections")
-        for d in prevDetections:
-            match = next((x for x in detections if d["id"] == x["id"]), None)
-            print(f"match {match}")
+        for d in detections:
+            match = next((x for x in newDetectionsList if d["id"] == x["id"]), None)
             if match == None:
                 newDetectionsList.append(d)
-            else:
-                # Merge the two, letting the newer one overrite the old
-                newDetectionsList.append({**d, **match})
 
-    for d in detections:
-        match = next((x for x in newDetectionsList if d["id"] == x["id"]), None)
-        if match == None:
-            newDetectionsList.append(d)
+        transaction.update(
+            audioRef,
+            {
+                "analysesPerformed": analysesPerformed,
+                "detections": newDetectionsList,
+                "hasDetections": len(newDetectionsList) > 0,
+            },
+        )
 
-    transaction.update(
-        audioRef,
-        {
-            "analysesPerformed": analysesPerformed,
-            "detections": newDetectionsList,
-            "hasDetections": len(newDetectionsList) > 0,
-        },
-    )
+    def download_audio(self, analysis_id: str, audio_record: dict) -> str:
+        """
+        Downloads the audio file from cloud storage to a place locally.
 
+        Be sure to delete the file after processing.
 
-def downloadAudio(analysisId: str, audioRecord: dict) -> str:
-    """
-    Downloads the audio file from cloud storage to a place locally.
+        Will return the filename once download is complete
+        """
 
-    Be sure to delete the file after processing.
+        audio_id = audio_record["id"]
+        uri = audio_record["uri"]
 
-    Will return the filename once download is complete
-    """
+        destinationPath = f"tmp/{analysis_id}"
+        Path(destinationPath).mkdir(parents=True, exist_ok=True)
+        destinationFile = f"{destinationPath}/{audio_id}.mp3"
 
-    audio_id = audioRecord["id"]
-    uri = audioRecord["uri"]
+        print(f"Downloading {uri}")
 
-    destinationPath = f"tmp/{analysisId}"
-    Path(destinationPath).mkdir(parents=True, exist_ok=True)
-    destinationFile = f"{destinationPath}/{audio_id}.mp3"
+        client = storage.Client()
+        with open(destinationFile, "wb") as file_obj:
+            client.download_blob_to_file(uri, file_obj)
 
-    print(f"Downloading {uri}")
+        return destinationFile
 
-    client = storage.Client()
-    with open(destinationFile, "wb") as file_obj:
-        client.download_blob_to_file(uri, file_obj)
-
-    return destinationFile
-
-
-def deleteDownloadedAudio(filepath: str):
-    os.remove(filepath)
+    def delete_downloaded_audio(self, filepath: str):
+        os.remove(filepath)
