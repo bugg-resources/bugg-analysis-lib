a2e1838ef4e096170027d7a1ab39f824f5167e1a
Justyn Butler <commits@justyn.co>
2023-12-12 20:07:46 +0000

    feat: BuggMessageAnalyser class handles message setup/cleanup

--- bugg_analysis_lib/pubsub.py
+++ bugg_analysis_lib/pubsub.py
@@ -2,6 +2,8 @@ from concurrent.futures import TimeoutError
 
 from google.cloud import pubsub_v1
 
+from bugg_analysis_lib.firebase import BuggFirebase
+
 
 # The function formerly known as start()
 def init_subscription(subscription_id, analysis_id, on_message_cb):
@@ -27,3 +29,42 @@ def init_subscription(subscription_id, analysis_id, on_message_cb):
         except TimeoutError:
             streaming_pull_future.cancel()  # Trigger the shutdown.
             streaming_pull_future.result()  # Block until the shutdown is complete.
+
+
+class BuggMessageAnalyser:
+    """
+    A class to handle the analysis of a single message from Pub/Sub
+    """
+
+    def __init__(self, message, analysis_id: str, fb: BuggFirebase):
+        self.message = message
+        self.audio_file_path = None
+        self.analysis_id = analysis_id
+        self.fb = fb
+
+    def __enter__(self):
+        # Decode the audio ID
+        self.audio_id = self.message.data.decode("utf-8")
+
+        # Fetch the database record
+        self.audio_rec = self.fb.get_audio_db_record(self.audio_id)
+        if self.audio_rec is None:
+            print(f"Audio record not found for {self.audio_id}")
+            raise ValueError("Audio record not found")
+
+        # Download the audio file
+        self.audio_file_path = self.fb.download_audio(self.analysis_id, self.audio_rec)
+        return self
+
+    def submit_detections(self, detections):
+        # Add the detections to the database
+        self.fb.mark_analysis_complete(
+            analysis_id=self.analysis_id, audio_id=self.audio_id, detections=detections
+        )
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Cleanup: always delete the audio file and ack the message
+        # TODO: consider whether there are situations when we shouldn't ack
+        if self.audio_file_path:
+            self.fb.delete_downloaded_audio(self.audio_file_path)
+        self.message.ack()
